d90d58e4806378f04c96f93526319bcebf67be5a
Abolfazl Borhani <abolfazlborhani80@gmail.com>
2025-03-21 01:35:06 +0330

    Added basic project structure with CMake and main file.

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "fileorganizer.h"
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+    FileOrganizer w;
+    w.show();
+    return a.exec();
+}
